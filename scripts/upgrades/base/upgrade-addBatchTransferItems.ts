5441a31533c7a8ab729de6cd6580679532c0b0d8
Timidan <akinnusotutemitayodaniel@gmail.com>
2025-09-04 10:25:31 +0100

    use upgrade calldata

--- scripts/upgrades/base/upgrade-addBatchTransferItems.ts
+++ scripts/upgrades/base/upgrade-addBatchTransferItems.ts
@@ -18,6 +18,8 @@ import {
   TokenCommitmentData,
 } from "../../getTokenCommitmentData";
 import { PC_WALLET } from "../../geistBridge/paths";
+import { ItemsTransferFacet__factory } from "../../../typechain";
+import { ItemsTransferFacetInterface } from "../../../typechain/ItemsTransferFacet";
 
 export async function upgradeAddBatchTransferItems() {
   const c = await varsForNetwork(ethers);
@@ -33,6 +35,17 @@ export async function upgradeAddBatchTransferItems() {
   ];
   const joined = convertFacetAndSelectorsToString(facets);
 
+  const json: TokenCommitmentData = require("../../tokenCommitmentData.json");
+  const payload = buildBatchSafeTransferFromArgsFromJson(json);
+
+  const iface = new ethers.utils.Interface(
+    ItemsTransferFacet__factory.abi
+  ) as ItemsTransferFacetInterface;
+  const payload2 = iface.encodeFunctionData("batchExtractItemsFromDiamond", [
+    payload.tos,
+    payload.ids,
+    payload.values,
+  ]);
   const args: DeployUpgradeTaskArgs = {
     diamondOwner: "0x01F010a5e001fe9d6940758EA5e8c777885E351e",
     diamondAddress: c.aavegotchiDiamond!,
@@ -40,59 +53,11 @@ export async function upgradeAddBatchTransferItems() {
     useLedger: true,
     useMultisig: false,
     useRelayer: false,
+    initAddress: c.aavegotchiDiamond!,
+    initCalldata: payload2,
   };
 
   await run("deployUpgrade", args);
-
-  //add PC_WALLET as item and game manager
-
-  const signer = await getLedgerSigner(ethers);
-
-  let daoFacet = await ethers.getContractAt(
-    "contracts/Aavegotchi/facets/DAOFacet.sol:DAOFacet",
-    c.aavegotchiDiamond!,
-    signer
-  );
-
-  //transfer tokens to owners
-
-  const json: TokenCommitmentData = require("../../tokenCommitmentData.json");
-  const payload = buildBatchSafeTransferFromArgsFromJson(json);
-
-  let itemsTransferFacet = await ethers.getContractAt(
-    "contracts/Aavegotchi/facets/ItemsTransferFacet.sol:ItemsTransferFacet",
-    c.aavegotchiDiamond!,
-    signer
-  );
-
-  let testing = ["hardhat", "localhost"].includes(network.name);
-  if (testing) {
-    const owner = await diamondOwner(c.aavegotchiDiamond!, ethers);
-    itemsTransferFacet = await impersonate(
-      owner,
-      itemsTransferFacet,
-      ethers,
-      network
-    );
-
-    daoFacet = await impersonate(owner, daoFacet, ethers, network);
-  }
-
-  //add PC_WALLET as game and item manager
-  let tx = await daoFacet.addGameManagers([PC_WALLET], [1000000]);
-  console.log("addGameManager tx:", tx.hash);
-  await tx.wait();
-  tx = await daoFacet.addItemManagers([PC_WALLET]);
-  console.log("addItemManager tx:", tx.hash);
-  await tx.wait();
-
-  tx = await itemsTransferFacet.batchExtractItemsFromDiamond(
-    payload.tos,
-    payload.ids,
-    payload.values
-  );
-  console.log("batchExtractItemsFromDiamond tx:", tx.hash);
-  await tx.wait();
 }
 
 if (require.main === module) {
